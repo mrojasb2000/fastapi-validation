b563830dfc97596861739b1f18b9077553d12c78
Mauricio Rojas Barrientos <maalroja@bancolombia.com.co>
2023-07-24 17:30:58 -0400

    feat: add get movie by ID

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 app = FastAPI()
+
+movies = [
+    {
+        "id": 1,
+        "title": "The last of us",
+        "rating": 9.2,
+        "age": 2023,
+        "description": "After a global pandemic destroys civilization ...",
+        "category": "fantasy",
+    },
+    {
+        "id": 2,
+        "title": "The Flash",
+        "rating": 8.5,
+        "age": 2020,
+        "description": "Narry Allen uses his super spped to change ...",
+        "category": "action",
+    },
+]
 
 
 @app.get("/")
 def root():
     """Root function"""
     return "Welcome FastAPI"
+
+
+@app.get("/movies", tags=["movies"])
+def get_movies():
+    """Movie list"""
+    return movies
+
+
+@app.get("/movies/{id}", tags=["movies"])
+def get_movieById(id: int = Path(ge=0, le=1_000)):
+    return [item for item in movies if item["id"] == id]
